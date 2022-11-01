9cb6aa0f95d7f4b5107ad35ba6a5d4e9a2719f95
yooveloper <yooveloper@gmail.com>
2022-11-01 16:58:47 +0900

    :add: calculator 클래스를 활용해서 기본적인 test 작성해보기

---
+++ src/calculator.js
@@ -0,0 +1,32 @@
+class Calculator {
+  constructor() {
+    this.value = 0;
+  }
+
+  set(num) {
+    this.value = num;
+  }
+
+  clear() {
+    this.value = 0;
+  }
+
+  add(num) {
+    this.value = this.value + num;
+  }
+
+  subtract(num) {
+    this.value = this.value - num;
+  }
+
+  multiply(num) {
+    this.value = this.value * num;
+  }
+
+  divide(num) {
+    this.value = this.value / num;
+  }
+  
+}
+
+module.exports = Calculator
---
+++ src/test/calculator.test.js
@@ -0,0 +1,61 @@
+const Calculator = require('../calculator.js');
+
+// describe : 관련있는 테스트들을 하나의 그룹으로 만든다
+describe('Calculator', () => {
+
+  let cal;
+  beforeEach(() => {
+    // 객체의 인스턴스를 테스트마다 계속 생성해줘서 중복코드가 발생하므로 
+    // beforeEach를 활용해서 선언해준다
+    cal = new Calculator();
+  })
+
+  // it : Calculator를 가르키는 3인칭
+  it('inits whth 0', () => {
+    expect(cal.value).toBe(0);
+  });
+
+  it('sets', () => {
+    cal.set(9);
+    expect(cal.value).toBe(9);
+  })
+
+  it('clear', () => {
+    cal.set(9)
+    cal.clear()
+    expect(cal.value).toBe(0);
+  })
+
+  it('adds', () => {
+    cal.set(1)
+    cal.add(2)
+    expect(cal.value).toBe(3);
+  })
+
+  it('subtracts', () => {
+    cal.subtract(1);
+    expect(cal.value).toBe(-1)
+  })
+
+  it('multiplies', () => {
+    cal.set(5);
+    cal.multiply(4);
+    expect(cal.value).toBe(20)
+  })
+
+  // describe 안에 describe를 써서 소그룹화 시킬 수 있음
+  describe('divdes', () => {
+    it('0 / 0 === NaN', () => {
+      cal.divide(0);
+      expect(cal.value).toBe(NaN);
+    })
+
+    it('1 / 0 === Infinity', () => {
+      cal.set(1)
+      cal.divide(0);
+      expect(cal.value).toBe(Infinity);
+    })
+  })
+
+
+}) 
