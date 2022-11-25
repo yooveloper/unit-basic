c450bb4fc585b5e96b405da77efb1dc27edc0ab2
yooveloper <yooveloper@gmail.com>
2022-11-25 20:56:38 +0900

    ✅  stack 자료구조 리펙터링

--- src/stack/stack.js
+++ src/stack/stack.js
@@ -1,29 +1,35 @@
 class Stack {
   constructor() {
-    this.array = [];
+    this._size = 0;
+    this.head = null;
   }
 
   size() {
-    return this.array.length;
+    return this._size;
   }
 
   push(item) {
-    this.array.push(item);
+    const node = { item, next: this.head };
+    this.head = node;
+    this._size++;
   }
 
   pop() {
-    if (this.array.length === 0) {
+    if (this.head === null) {
       throw new Error('스택이 비어있음');
     }
 
-    return this.array.pop();
+    const node = this.head;
+    this.head = node.next;
+    this._size--;
+    return node.item;
   }
 
   peek() {
-    if (this.array.length === 0) {
+    if (this.head === null) {
       throw new Error('스택이 비어있음');
     }
-    return this.array[this.size() - 1];
+    return this.head.item;
   }
 }
 
